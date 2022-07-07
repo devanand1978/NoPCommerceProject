348e328bccdfa2056f7f4254cfc0ef0d7ca8e7b6
Devanand Kumar <devrishi.dk@gmail.com>
2022-07-08 00:25:40 +0530

    Added new file

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 ECHO "RUNIING TESTCASES....."
 pytest -s -v -m "sanity and regression" --html=./Reports/report.html TestCases/ --browser=chrome
-pytest -s -v -m "sanity and regression" --html=./Reports/report.html TestCases/ --browser=firefox
+REM pytest -s -v -m "sanity and regression" --html=./Reports/report.html TestCases/ --browser=firefox
 REM pytest -s -v -m "sanity" --html=./Reports/report.html TestCases/ --browser=chrome
 REM pytest -s -v -m "regression" --html=./Reports/report.html Testcases/ --browser=chrome
 REM pytest -s -v -m "sanity or regression" --html=./Reports/report.html Testcases/ --browser=chrome
